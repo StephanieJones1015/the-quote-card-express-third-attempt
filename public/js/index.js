1a448af12c858d9880ebfd4c69641c3bebb816cf
Stephanie Jones <stephaniewilliams062715@gmail.com>
2025-06-16 20:39:52 -0400

    added code as directed in the directions

---
+++ public/js/index.js
@@ -0,0 +1,51 @@
+"use strict"
+
+const elements = {
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+};
+
+async function getRandomImage() {
+    const client_id = "qyrjw6HNMhP2c3Q6UDNoM5d5PKSExevkwDrVWy2YJcI";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json()
+        console.log(returnedData)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
+/*const quotes = [
+    {
+        quote: "All hands! Abandon ship!",
+        author: "Captain Picard",
+    },
+
+    {
+        quote: "Doh!",
+        author: "Homer Simpson",
+    },
+
+    {
+        quote: "The Journey of a Thousand Miles Begins with One Step",
+        author: "Lao Tzu",
+    }
+]
+function loopThroughQuotes() {
+    let quoteIndex = 0;
+    setInterval(() => {
+        if (quoteIndex < quotes.length) {
+            elements.quote.textContent = quotes[quoteIndex].quote;
+            elements.author.textContent = quotes[quoteIndex].author;
+            quoteIndex++;
+        } else {
+            quoteIndex = 0;
+        }
+    }, 3000);
+}
+setTimeout(loopThroughQuotes, 3000); */
+
+getRandomImage();
