207677e20fd5e89894996586e99569e56ec0a49c
Stephanie Jones <stephaniewilliams062715@gmail.com>
2025-06-17 12:35:26 -0400

    corrected an error system is picking up

--- public/js/index.js
+++ public/js/index.js
@@ -8,7 +8,7 @@ const elements = {
 };
 
 async function getRandomImage() {
-    const client_id = 
+    const client_id = "YOUR_UNSPLASH_ACCESS_KEY";
     const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
     try {
         const response = await fetch(endpoint);
