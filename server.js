73b4e687480f37fea9a4c889e0288742435946cb
Stephanie Jones <stephaniewilliams062715@gmail.com>
2025-06-17 22:36:15 -0400

    corrected an error in server.js

--- server.js
+++ server.js
@@ -37,7 +37,12 @@ app.use("/api/v1/getRandomImage", async (request, response) => {
     });
 });
 
-
+app.use("/api/v1/getRandomImage", (request, response) => {
+    response.status(200).json({
+        status: 200,
+        data: process.env.CLIENT_ID
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server is running http://localhost:${port}`);
